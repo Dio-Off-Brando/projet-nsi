7b63c0973a2b8e74285fc584e2375043be4b7b67
Mehdi Ben Ahmed <somedudz@protonmail.com>
2021-10-18 19:53:17 +0200

    idk

--- main.py
+++ main.py
@@ -28,8 +28,20 @@ def affichage_allumettes(liste):
 def initialisation(liste=[1,2,3,4,5,6], liste2=[1,2,3,4,5,6]):
     joueur1=input("nom joueur1: ")
     joueur2=input('nom joueur2: ')
+    total_alu=0
+    for i in range(liste):
+        total_alu = total_alu+ liste[i]*liste2[i]
+    
+    #retourner une liste avec les donnees [joueur1, joueur2, liste ,liste2, total_alu]
+    return [joueur1, joueur2, liste, liste2, total_alu]
 
 
+def main():
+    joueur1, joueur2, liste, liste2, total_alu = initialisation()
+    
+    dernier_joueur = ""
+    while total_alu <0:
+        tas = choix_tas()
+        nb_alu = choix_allumettes()
 
-def main()
-    pass
+        

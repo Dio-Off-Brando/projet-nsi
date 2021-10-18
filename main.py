a87da1a4760630ad2b1a2c32493f3d6808403161
Mehdi Ben Ahmed <somedudz@protonmail.com>
2021-10-18 19:28:49 +0200

    added functions?

--- main.py
+++ main.py
@@ -12,9 +12,24 @@
 def affichage_tas(liste):
     pass
 
+def choix_allumettes():
+    return input("choisis un nombre d'allumettes: ")
+
+def choix_tas():
+    return input("choisis un tas")
+
+
 def affichage_allumettes(liste):
     pass
 
 
+# fonction principale
+
+def initialisation(liste=[1,2,3,4,5,6], liste2=[1,2,3,4,5,6]):
+    joueur1=input("nom joueur1: ")
+    joueur2=input('nom joueur2: ')
 
 
+
+def main()
+    pass

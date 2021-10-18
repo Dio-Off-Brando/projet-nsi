7c5825b565d587bb07f6e2b937e2e3c5f420bbf2
Mehdi Ben Ahmed <somedudz@protonmail.com>
2021-10-18 22:20:36 +0200

    je ne sais pas ce que je suis en train de faire

--- main.py
+++ main.py
@@ -12,11 +12,14 @@
 def affichage_tas(liste):
     pass
 
-def choix_allumettes():
-    return input("choisis un nombre d'allumettes: ")
+def choix_allumettes(joueur):
+    #adapter au nom du joueur
+    return input(joueur+" choisis un nombre d'allumettes: ")
 
-def choix_tas():
-    return input("choisis un tas")
+
+def choix_tas(joueur):
+    #adapter au nom du joueur
+    return input(joueur+" choisis un tas")
 
 
 def affichage_allumettes(liste):
@@ -40,12 +43,21 @@ def initialisation(liste=[1,2,3,4,5,6], liste2=[1,2,3,4,5,6]):
 
 def main():
     joueur1, joueur2, liste, liste2, total_alu = initialisation()
-    
-    dernier_joueur = ""
+    # 0 veut dire joueur1, 1 veut dire joueur2
+    # mit a 1 pour commencer avec le joueur1
+    dernier_joueur = 1
+    joueur=0
+
     while total_alu <0:
-        affichage_tas()
-        affichage_allumettes()
-        tas = choix_tas()
-        nb_alu = choix_allumettes()
+        if dernier_joueur==1:
+            joueur=joueur1
+            dernier_joueur = 0
+        else:
+            joueur = joueur2
+            dernier_joueur=1
+        affichage_tas(liste)
+        affichage_allumettes(liste2)
+        tas = choix_tas(joueur1)
+        nb_alu = choix_allumettes(joueur1)
 
         

26c4c5fc315d6dd81f1d39e74941473f4ed779c1
Mehdi Ben Ahmed <somedudz@protonmail.com>
2021-10-18 19:06:36 +0200

    first commit/ setting required functions

---
+++ main.py
@@ -0,0 +1,20 @@
+##################
+#   jeu de nim   #
+#     Mehdi      #
+#       &        #
+#                #
+#                #
+##################
+
+
+
+
+def affichage_tas(liste):
+    pass
+
+def affichage_allumettes(liste):
+    pass
+
+
+
+
